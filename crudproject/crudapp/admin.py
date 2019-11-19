37dba6641c9da313fb647d3732fc4db2de236aef
yashika0810 <yashika0810>
2019-11-19 10:28:39 -0500

    first commit

---
+++ crudproject/crudapp/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from crudapp.models import Employee
+# Register your models here.
+
+admin.site.register(Employee)
---
+++ crudproject/crudapp/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from django.db import models
+from .models import Employee
+
+class EmployeeForm(forms.ModelForm):
+    class Meta:
+        model = Employee
+        fields = "__all__"
